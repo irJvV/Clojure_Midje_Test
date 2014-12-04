17dc89b1f593d5b327de6f4c52690888d56856c0
ir.joost.van.veen <ir.joost.van.veen@gmail.com>
2014-12-04 12:07:33 +0100

    added logging by timbre to log file

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject joost_midje_testing "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :main joost-midje-testing.core
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.taoensso/timbre "3.3.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
--- src/joost_midje_testing/core.clj
+++ src/joost_midje_testing/core.clj
@@ -1,6 +1,12 @@
-(ns joost-midje-testing.core)
+(ns joost-midje-testing.core (:require [taoensso.timbre :as timbre]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(timbre/refer-timbre)
+(timbre/set-config! [:appenders :spit :enabled?] true)
+(timbre/set-config! [:shared-appender-config :spit-filename] "log_files/joost_midje_testing_log.txt")
+
+
+(info "This will print")
+
+(spy :info (* 5 4 3 2 1))
+
+;;(info (Exception. "Oh noes") "arg1" "arg2")
